5db5e0ccacbe2811e2911451552bedc0916054cd
Abdul Rahman Al - Zeedi <aalzeedi85@gmail.com>
2026-07-07 08:19:00 +0400

    implemented problem 1 ( Personal info card )

--- Task 1 Solution/Program.cs
+++ Task 1 Solution/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //1- Personal Info Card
+            //
+            string name = "Sara";
+            int age = 21;
+            double height = 1.65;
+            bool student = true;
+
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Age: " + age);
+            Console.WriteLine("Height: " + height);
+            Console.WriteLine("Student: " + student);
+            //////////////////////////////////////////
         }
     }
 }
