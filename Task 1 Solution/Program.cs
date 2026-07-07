0eb4fb0a762a414acef2c33ffa818b681e72a3a5
Abdul Rahman Al - Zeedi <aalzeedi85@gmail.com>
2026-07-07 08:21:57 +0400

    implemented problem 2 ( Rectangle Calculator )

--- Task 1 Solution/Program.cs
+++ Task 1 Solution/Program.cs
@@ -16,6 +16,22 @@
             Console.WriteLine("Height: " + height);
             Console.WriteLine("Student: " + student);
             //////////////////////////////////////////
+            ///
+
+            //2- Rectangle Calculator
+            //
+            Console.Write("Enter length: ");
+            double length = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter width: ");
+            double width = Convert.ToDouble(Console.ReadLine());
+
+            double area = length * width;
+            double perimeter = 2 * (length + width);
+
+            Console.WriteLine("Area: " + area);
+            Console.WriteLine("Perimeter: " + perimeter);
+            /////////////////////////////////////////
         }
     }
 }
