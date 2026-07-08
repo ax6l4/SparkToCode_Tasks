0d95531bc0a8f85c41901fe964760b182a58f7d2
Abdul Rahman Al - Zeedi <aalzeedi85@gmail.com>
2026-07-08 13:46:29 +0400

    add project files

---
+++ Task 2 Solution/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Task_2_Solution
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
