280fbc4861154bc5d4856ab82d53e8a37ea1f640
Abdul Rahman Al - Zeedi <aalzeedi85@gmail.com>
2026-07-08 21:53:51 +0400

    implemented problem 5 ( Grade Rounding System )

--- Task 3 Solution/Program.cs
+++ Task 3 Solution/Program.cs
@@ -4,45 +4,23 @@
     {
         static void Main(string[] args)
         {
-            // Task 1 - Absolute Difference
-            Console.Write("Enter first number: ");
-            double num1 = Convert.ToDouble(Console.ReadLine());
+            // Task 5 - Grade Rounding System
+            Console.Write("Enter exam score: ");
+            double score = Convert.ToDouble(Console.ReadLine());
 
-            Console.Write("Enter second number: ");
-            double num2 = Convert.ToDouble(Console.ReadLine());
+            double roundedScore = Math.Round(score);
 
-            double difference = Math.Abs(num1 - num2);
+            Console.WriteLine("Rounded Score: " + roundedScore);
 
-            Console.WriteLine("Positive Difference: " + difference);
-            /////////////////////////////////
-            ///
-
-
-            // Task 2 - Power & Root Explorer
-            Console.Write("Enter a number: ");
-            double number = Convert.ToDouble(Console.ReadLine());
-
-            double square = Math.Pow(number, 2);
-            double root = Math.Sqrt(number);
-
-            Console.WriteLine("Square: " + square);
-            Console.WriteLine("Square Root: " + root);
-            /////////////////////////////////
-            ///
-
-
-            //Task 3 - Name Formatter
-            Console.Write("Enter your full name: ");
-            string name = Console.ReadLine();
-
-            Console.WriteLine("Uppercase: " + name.ToUpper());
-            Console.WriteLine("Lowercase: " + name.ToLower());
-            Console.WriteLine("Number of characters: " + name.Length);
-            ///////////////////////////////////
-            ///
-
-
-            
+            if (roundedScore >= 60)
+            {
+                Console.WriteLine("Result: Pass");
+            }
+            else
+            {
+                Console.WriteLine("Result: Fail");
+                /////////////////////////////////////
+                ///
 
 
 
@@ -51,6 +29,9 @@
 
 
 
-        }
+
+
+
+            }
     }
 }
