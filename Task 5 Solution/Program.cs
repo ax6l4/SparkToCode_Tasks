a74617a1d0b85d2225a5198c0622d2c8d53df3cb
Abdul Rahman Al - Zeedi <aalzeedi85@gmail.com>
2026-07-09 21:32:50 +0400

    implemented problem 1 (  Fixed Grades Array )

--- Task 5 Solution/Program.cs
+++ Task 5 Solution/Program.cs
@@ -4,7 +4,28 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Task 1 - Fixed Grades Array
+            static void Task1()
+            {
+                Console.WriteLine("\nTask 1 - Grades");
+
+                int[] studentGrades = new int[5];
+
+                for (int gradeIndex = 0; gradeIndex < studentGrades.Length; gradeIndex++)
+                {
+                    Console.Write("Enter grade: ");
+                    studentGrades[gradeIndex] = Convert.ToInt32(Console.ReadLine());
+                }
+
+                Console.WriteLine("Grades:");
+
+                foreach (int currentGrade in studentGrades)
+                {
+                    Console.WriteLine(currentGrade);
+                }
+            }
+            ///////////////////////////////////////
+            ///
         }
     }
 }
