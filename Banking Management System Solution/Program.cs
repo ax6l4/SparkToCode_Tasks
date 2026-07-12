0ada032a0bdfefb60f8f452bcea3b9186f7e2c2b
Abdul Rahman Al - Zeedi <aalzeedi85@gmail.com>
2026-07-12 10:45:00 +0400

    add project files

---
+++ Banking Management System Solution/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Banking_Management_System_Solution
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
