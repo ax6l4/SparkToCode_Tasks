8919f9c1e87046dec165420580d7beef5c84dc34
Abdul Rahman Al - Zeedi <aalzeedi85@gmail.com>
2026-07-13 11:56:07 +0400

    add methods

--- OOP Part 1 Solution/Program.cs
+++ OOP Part 1 Solution/Program.cs
@@ -1,15 +1,53 @@
-﻿namespace OOP_Part_1_Solution
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace OOP_Part_1_Solution
 {
+
+    
     // Add class BankAccount with the following properties and methods:
-    class BankAccount
+    public class BankAccount
     {
         public int AccountNumber { get; set; }
         public string HolderName { get; set; }
         public double Balance { get; set; }
+        private string email { get; set; }
+
+        //methods
+        public void Deposit(double amount)
+        {
+            Balance += amount;
+        }
+        public void wirhdraw(double amount)
+        {
+            if (amount <= Balance)
+            {
+                Balance -= amount;
+            }
+            else
+            {
+                Console.WriteLine("Insufficient balance.");
+            }
+        }
+       public double CheckBalance()
+        {
+            return Balance;
+        }
+        private void printInformation()
+        {
+            Console.WriteLine("Account Number: " + AccountNumber);
+            Console.WriteLine("Holder Name: " + HolderName);
+            Console.WriteLine("Balance: " + Balance);
+        }
+        private void sendEmail()
+        {
+            Console.WriteLine("Sending email to " + email);
+        }
+
 
     }
 
-        public class Program
+
+    public class Program
     {
 
 
@@ -17,14 +55,13 @@
         {
             //Create an object with the class BankAccount
             //
-            BankAccount account = new BankAccount();
-            account.AccountNumber = 123456;
-            account.HolderName = "John Doe";
-            account.Balance = 1000.0;
-            // Display the account information
-            Console.WriteLine("Account Number: " + account.AccountNumber);
-            Console.WriteLine("Holder Name: " + account.HolderName);
-            Console.WriteLine("Balance: $" + account.Balance);
+            BankAccount B1 = new BankAccount();
+            B1.AccountNumber= 1163;
+            B1.HolderName = "karim";
+            B1.Balance = 120;
+            
+            double result = B1.CheckBalance();
+
 
         }
 
