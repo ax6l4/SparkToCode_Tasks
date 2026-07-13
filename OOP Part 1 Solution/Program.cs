aab58a4a237070eabfa98903dc6ce3ed96884276
Abdul Rahman Al - Zeedi <aalzeedi85@gmail.com>
2026-07-13 10:52:11 +0400

    add object with the class

--- OOP Part 1 Solution/Program.cs
+++ OOP Part 1 Solution/Program.cs
@@ -1,10 +1,42 @@
 ﻿namespace OOP_Part_1_Solution
 {
+    // Add class BankAccount with the following properties and methods:
     class BankAccount
     {
+        public int AccountNumber { get; set; }
+        public string HolderName { get; set; }
+        public double Balance { get; set; }
+
+    }
+
+        public class Program
+    {
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Create an object with the class BankAccount
+            //
+            BankAccount account = new BankAccount();
+            account.AccountNumber = 123456;
+            account.HolderName = "John Doe";
+            account.Balance = 1000.0;
+            // Display the account information
+            Console.WriteLine("Account Number: " + account.AccountNumber);
+            Console.WriteLine("Holder Name: " + account.HolderName);
+            Console.WriteLine("Balance: $" + account.Balance);
+
         }
+
+
+
     }
 }
+
+
+
+
+
+
+
+
