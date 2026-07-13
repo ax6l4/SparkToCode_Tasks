eab2222bfd4100887f3120a96b555ebfacc0f322
Abdul Rahman Al - Zeedi <aalzeedi85@gmail.com>
2026-07-13 05:54:00 +0400

    add files

---
+++ OOP Part 1 Solution/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace OOP_Part_1_Solution
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
