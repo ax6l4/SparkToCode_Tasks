7b4d6fe8c94d3e0a43a49d86e1291a098be3c96b
Abdul Rahman Al - Zeedi <aalzeedi85@gmail.com>
2026-07-16 10:14:52 +0400

    Solution for Register New Guest

--- Hotel Management System/Program.cs
+++ Hotel Management System/Program.cs
@@ -16,79 +16,199 @@
             IsAvailable = true;
         }
     }
+
+    class Guest
+    {
+        public string GuestId { get; set; }
+        public string GuestName { get; set; }
+        public string RoomNumber { get; set; }
+        public string CheckInDate { get; set; }
+        public int TotalNights { get; set; }
+
+
+        public Guest(string id, string name, string date, int nights)
+        {
+            GuestId = id;
+            GuestName = name;
+            RoomNumber = "Not Assigned";
+            CheckInDate = date;
+            TotalNights = nights;
+        }
+    }
     internal class Program
     {
 
         static void Main(string[] args)
         {
             List<Room> rooms = new List<Room>();
-            { 
+            List<Guest> guests = new List<Guest>();
 
-                while (true)
+
+            while (true)
+            {
+                Console.WriteLine("==============================");
+                Console.WriteLine(" HOTEL MANAGEMENT SYSTEM");
+                Console.WriteLine("==============================");
+
+                Console.WriteLine("1. Add New Room");
+                Console.WriteLine("2. Register New Guest");
+                Console.WriteLine("0. Exit");
+
+
+                Console.Write("Enter your choice: ");
+
+                int choice = int.Parse(Console.ReadLine());
+
+
+                switch (choice)
                 {
-                    Console.WriteLine("====== HOTEL MANAGEMENT SYSTEM ======");
-                    Console.WriteLine("1. Add New Room");
-                    Console.WriteLine("0. Exit");
 
-                    Console.Write("Enter your choice: ");
+                    // Add new room 
+                    case 1:
 
-                    int choice = int.Parse(Console.ReadLine());
+                        Console.WriteLine("\n=== Add New Room ===");
 
 
-                    switch (choice)
-                    {
-                        case 1:
-
-                            Console.WriteLine("=== Add New Room ===");
-
-                            Console.Write("Enter Room Number: ");
-                            int roomNumber = int.Parse(Console.ReadLine());
+                        Console.Write("Enter Room Number: ");
+                        int roomNumber;
 
 
-                            bool roomExists = rooms.Any(r => r.RoomNumber == roomNumber);
+                        while (!int.TryParse(Console.ReadLine(), out roomNumber) || roomNumber <= 0)
+                        {
+                            Console.Write("Invalid Room Number. Enter again: ");
+                        }
 
 
-                            if (roomExists)
-                            {
-                                Console.WriteLine("Room already exists!");
-                                break;
-                            }
+
+                        bool roomExists = rooms.Any(r => r.RoomNumber == roomNumber);
 
 
-                            Console.Write("Enter Room Type: ");
-                            string roomType = Console.ReadLine();
 
-
-                            Console.Write("Enter Price Per Night: ");
-                            double price = double.Parse(Console.ReadLine());
-
-
-                            Room newRoom = new Room(roomNumber, roomType, price);
-
-
-                            rooms.Add(newRoom);
-
-
-                            Console.WriteLine("Room Added Successfully!");
-                            Console.WriteLine("Total Rooms: " + rooms.Count);
-
+                        if (roomExists)
+                        {
+                            Console.WriteLine("Room already exists!");
                             break;
+                        }
 
 
-                        case 0:
-                            return;
+
+                        Console.Write("Enter Room Type (Single / Double / Suite): ");
+                        string roomType = Console.ReadLine();
 
 
-                        default:
-                            Console.WriteLine("Invalid Choice");
-                            break;
-                    }
+
+                        Console.Write("Enter Price Per Night: ");
+                        double price;
+
+
+
+                        while (!double.TryParse(Console.ReadLine(), out price) || price <= 0)
+                        {
+                            Console.Write("Invalid Price. Enter again: ");
+                        }
+
+
+
+                        Room newRoom = new Room(roomNumber, roomType, price);
+
+
+
+                        rooms.Add(newRoom);
+
+
+
+                        Console.WriteLine("\nRoom Added Successfully!");
+                        Console.WriteLine("Room Number: " + newRoom.RoomNumber);
+                        Console.WriteLine("Room Type: " + newRoom.RoomType);
+                        Console.WriteLine("Price: " + newRoom.PricePerNight);
+                        Console.WriteLine("Total Rooms: " + rooms.Count);
+
+
+                        break;
+
+
+
+                    // Add register New Guest
+                    case 2:
+
+
+                        Console.WriteLine("\n=== Register New Guest ===");
+
+
+
+                        Console.Write("Enter Guest Name: ");
+                        string guestName = Console.ReadLine();
+
+
+
+                        Console.Write("Enter Check-In Date: ");
+                        string checkInDate = Console.ReadLine();
+
+
+
+                        Console.Write("Enter Number of Nights: ");
+                        int nights;
+
+
+
+                        while (!int.TryParse(Console.ReadLine(), out nights) || nights <= 0)
+                        {
+                            Console.Write("Invalid Nights. Enter again: ");
+                        }
+
+
+
+                        string guestId = "G" + (guests.Count + 1).ToString("D3");
+
+
+
+                        Guest newGuest = new Guest(
+                            guestId,
+                            guestName,
+                            checkInDate,
+                            nights
+                        );
+
+
+
+                        guests.Add(newGuest);
+
+
+
+                        Console.WriteLine("\nGuest Registered Successfully!");
+                        Console.WriteLine("Guest ID: " + newGuest.GuestId);
+                        Console.WriteLine("Name: " + newGuest.GuestName);
+                        Console.WriteLine("Room: " + newGuest.RoomNumber);
+                        Console.WriteLine("Nights: " + newGuest.TotalNights);
+
+
+
+                        break;
+
+
+
+                    case 0:
+
+                        return;
+
+
+
+                    default:
+
+                        Console.WriteLine("Invalid Choice");
+
+                        break;
+
                 }
+
             }
-
-
-
 
         }
     }
 }
+    
+
+
+
+
+
